13d886428d7bf8645cfdc0d2913a44ee0e2b0cff
Marchenko Sergey <mse08061993@gmail.com>
2023-10-30 18:07:13 +0300

    Add front controller

--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,14 +1,3 @@
-<?php
+<?php $name = $request->query->get('name', 'World'); ?>
 
-require_once __DIR__ . '/vendor/autoload.php';
-
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
-$request = Request::createFromGlobals();
-$response = new Response();
-
-$name = $request->query->get('name', 'World');
-$response->setContent(sprintf("Hello, %s", htmlspecialchars($name, encoding: 'utf-8')));
-
-$response->send();
+Hello, <?= htmlspecialchars($name, encoding: 'UTF-8') ?>
--- web/index.php
+++ web/index.php
@@ -1,14 +1,29 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+define('PAGES_DIR', __DIR__ . '/../src/pages/');
+
 $request = Request::createFromGlobals();
 $response = new Response();
 
-$name = $request->query->get('name', 'World');
-$response->setContent(sprintf("Hello, %s", htmlspecialchars($name, encoding: 'utf-8')));
+$path = $request->getPathInfo();
+
+$map = [
+    '/hello' => PAGES_DIR . 'hello.php',
+    '/goodbay' => PAGES_DIR . 'goodbay.php',
+];
+
+if (isset($map[$path])) {
+    ob_start();
+    include $map[$path];
+    $response->setContent(ob_get_clean());
+} else {
+    $response->setContent('Page not found');
+    $response->setStatusCode(404);
+}
 
 $response->send();
